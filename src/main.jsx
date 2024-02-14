a3eea574d1fde8c20de2be9fe6c91077483ef23b
Ben Derham <1805685+benderham@users.noreply.github.com>
2024-02-14 09:12:50 +0800

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <NavBar />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/project', element: <ProjectPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
